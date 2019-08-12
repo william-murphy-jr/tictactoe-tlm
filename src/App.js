5b40b60d5c965b3e4d0f50830e8af7d6139a1db0
Bill Murphy <bill559m@gmail.com>
2019-08-11 21:05:13 -0700

    working basic game with one state  array for squares

--- src/App.js
+++ src/App.js
@@ -4,15 +4,81 @@ import './App.css';
 import Board from './components/Board';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      }
+  }
+  
+
+  calculateWinner(squares) {
+    // console.log('calculateWinner');
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+
+      // We can not just use:  squares[a] === squares[b] && squares[a] === squares[c]
+      // What would happen if all values are null so we would have
+      // null === null && null === null => which evaluate to true and the game clearly has 
+      // not been won at this point.
+      // So the "squares[a] &&" at the beginning of the expression
+      // will short-circuit the rest of the evaluation since null is a false value.
+      // We would have:
+      // null   && null === null && null === null
+      // false  &&      true     &&      true 
+      // which is false;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const winner = this.calculateWinner(this.state.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <h1 className="game-title">Tic Tac Toe</h1>
-          <Board />
+        <h1 className="game-title">Tic Tac Toe</h1>
+          <Board
+            squares={this.state.squares}
+            handleClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,100 +1,34 @@
 import React from 'react';
 import Square from './Square';
 
-class Board extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-
-  }
-
-  calculateWinner(squares) {
-    console.log('calculateWinner');
-    const winCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < winCombos.length; i++) {
-      const [a, b, c] = winCombos[i];
-
-      // We can not just use:  squares[a] === squares[b] && squares[a] === squares[c]
-      // What would happen if all values are null so we would have
-      // null === null && null === null => which evaluate to true and the game clearly has 
-      // not been won at this point.
-      // So the squares[a] && at the beginning of the expression
-      // will short-circuit the rest of the evaluation since null is a false value.
-      // We would have:
-      // null   && null === null && null === null
-      // false  &&      true     &&      true 
-      // which is false;
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-  
-  // This function helps clean the board code up
-  renderSquare(i) {
+const Board = (props) => {
+  // This function helps clean the boards Square code up
+  const renderSquare = (i) => {
     return <Square
-      value = { this.state.squares[i] }
-      handleClick = { () => { this.handleClick(i) } }
+      value = { props.squares[i] }
+      handleClick = { () => { props.handleClick(i) } }
     />;
   }
   
-  render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status;
-    
-    if (winner) {
-      status = `Winner is:  ${winner}`;
-    } else {
-      status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
